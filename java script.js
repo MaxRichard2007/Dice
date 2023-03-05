06741244683770e61ee9e447025ca0d3f5529721
Maxrichard2007 <max536854@gmail.com>
2023-03-05 19:28:40 +0330

    dice

--- java script.js
+++ java script.js
@@ -1,23 +1,24 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;//1-6
 
-var randomDiceImge = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImgeSource = "images/" + randomDiceImge; //images/dice1.png - image/images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - image/images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImgeSource);
+image1.setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;//1-6
 
-var randomDiceImge = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
 
-var randomImgeSource = "images/" + randomDiceImge; //images/dice1.png - image/images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - image/images/dice6.png
 
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", randomImgeSource);
+image2.setAttribute("src", randomImageSource);
+
 
 //if player 1 wins
 if (randomNumber1 > randomNumber2) {
