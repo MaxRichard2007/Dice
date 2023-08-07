ac9f32f35b85b7962bd4955a11b1f7cc2ca9822f
Maxrichard2007 <max536854@gmail.com>
2023-08-07 10:37:39 +0330

    Dice

--- javascript.js
+++ javascript.js
@@ -2,22 +2,22 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;//1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - image/images/dice6.png
+var randomimageource = "image/" + randomDiceImage; //image/dice1.png - image/image/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomimageource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;//1-6
 
 var randomDiceImage = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - image/images/dice6.png
+var randomimageource = "image/" + randomDiceImage; //image/dice1.png - image/image/dice6.png
 
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", randomImageSource);
+image2.setAttribute("src", randomimageource);
 
 
 //if player 1 wins
